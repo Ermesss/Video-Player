85434f1ba005cf6a61376b52c7ef7a20ff0ae5a9
Ermanno Fissore <ermannofissore@gmail.com>
2022-09-29 18:31:06 +0200

    Video editor appear but video doesn't start Need to investigate

--- Video Player/ContentView.swift
+++ Video Player/ContentView.swift
@@ -6,21 +6,39 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            player().frame(height: UIScreen.main.bounds.height / 2.3)
+            Spacer()
+            
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
+    
+    struct player : UIViewControllerRepresentable {
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<player>) -> AVPlayerViewController {
+            
+            let controller = AVPlayerViewController()
+            let url = "https://youtu.be/zGe6Dy-JRNI"
+            let player1 = AVPlayer(url: URL(string: url)!)
+            controller.player = player1
+            return controller
+        }
+        
+        func updateUIViewController(_ uiViewController: AVPlayerViewController, context: UIViewControllerRepresentableContext<player>) {
+            
+        }
+        
     }
 }
