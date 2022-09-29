64ebb91c90a0e8d133c2b883a349f2e77be88cc2
Ermanno Fissore <ermannofissore@gmail.com>
2022-09-29 15:20:01 +0200

    Initial Commit

---
+++ Video Player/Video_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video_PlayerApp.swift
+//  Video Player
+//
+//  Created by Ermanno Fissore on 29/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Video_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
